ae64fb4755a5f31b966fca709f279328c80f060d
Eneroh <28052446+eneroh@users.noreply.github.com>
2023-01-13 14:53:05 +1100

    change to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-int main()
-{
-	cout << "Hello World!" << endl;
+int main() {
+	std::cout << "Hello World!" << std::endl;
 
 	return 0;
 }
