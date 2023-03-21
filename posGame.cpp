18001b178c54891590e441f66aa657a352a96565
eneroh <28052446+eneroh@users.noreply.github.com>
2023-03-22 10:41:05 +1100

    Create posGame.cpp

---
+++ posGame.cpp
@@ -0,0 +1,9 @@
+int main() {
+ int pos = 0;
+ int endPos = 0;
+ while (pos < endPos) {
+   pos++;
+   std::cout << "Current position is " << pos << std::endl;
+ }
+ std::cout << "You have reached the end!" << std::endl;
+}
